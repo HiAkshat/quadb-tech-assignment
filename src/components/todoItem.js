198d48d0ee962252b670f44503b3c82c245dd241
Akshat Gupta <akshatgupta300@gmail.com>
2024-04-13 19:45:33 +0530

    added task completed toggle, delete functionality

--- src/components/todoItem.js
+++ src/components/todoItem.js
@@ -1,15 +1,36 @@
 import DoneIcon from '@mui/icons-material/Done';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { useDispatch } from 'react-redux';
+import { toggleComplete, deleteTodo } from '@/redux/todoSlice';
 
 export default function TodoItem({id, title, completed}) {
+  const dispatch = useDispatch()
+
+  const handleComplete = () => {
+    dispatch(
+      toggleComplete({
+        id
+      })
+    )
+  }
+
+  const handleDelete = () => {
+    dispatch(
+      deleteTodo({
+        id
+      })
+    )
+  }
+  
   return (
     <div>
       <div className="flex items-center gap-2">
         <div className="flex items-center px-3 py-1 gap-2 bg-[#444444] rounded-lg">
-          <div><DoneIcon /></div>
+          <button onClick={handleComplete}><DoneIcon /></button>
           <span>{title}</span>
+          {completed && <span>he</span>}
         </div>
-        <div><DeleteIcon /></div>
+        <button onClick={handleDelete}><DeleteIcon /></button>
       </div>
     </div>
   )
--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -5,7 +5,7 @@ const todoSlice = createSlice({
   initialState: [
     {id: 1, title: "todo1", completed: false},
     {id: 2, title: "todo2", completed: false},
-    {id: 3, title: "todo3", completed: false},
+    {id: 3, title: "todo3", completed: true},
   ],
   reducers: {
     addTodo: (state, action) => {
@@ -16,9 +16,16 @@ const todoSlice = createSlice({
       }
 
       state.push(newTodo)
+    },
+    toggleComplete: (state, action) => {
+      const index = state.findIndex(todo => todo.id===action.payload.id)
+      state[index].completed=!state[index].completed
+    },
+    deleteTodo: (state, action) => {
+      return state.filter(todo => todo.id !== action.payload.id)
     }
   }
 })
 
-export const { addTodo } = todoSlice.actions
+export const { addTodo, toggleComplete, deleteTodo } = todoSlice.actions
 export default todoSlice.reducer
